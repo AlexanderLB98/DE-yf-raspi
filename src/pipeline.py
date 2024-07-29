a40eccbbbd970d19041940eeab2cf11cf064a0f7
AlexanderLB98 <AlexanderBool98@gmail.com>
2024-07-29 20:56:18 +0200

    requirements.txt

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,9 +4,19 @@ Here I will generate the pipeline that should be done
 
 from src.yfmanager import Yfmanager
 from src.dbmanager import DbManager
+from src.aux.utils import DotDict
+
+import json
 
 def pipeline():
     
+    config_file ="config.json"
+
+    with open(config_file) as f:
+        config = DotDict(json.load(f))
+        print(config)
+        
+        
     db = DbManager()
     
     # First we should get a list with the companies from de DB 
@@ -14,8 +24,8 @@ def pipeline():
     # companies_list = ["^AEX", "TSLA", "NVDA"]
     
     # Then we should download the data from yfinance
-    yfmanager = Yfmanager()
-    df = yfmanager.download_companies_yf(companies=companies_list, dt_days=1)
+    yfmanager = Yfmanager(config=config)
+    df = yfmanager.download_companies_yf(companies=companies_list)
     
     # Now we have to upload the df to the database
     db.add_df_to_postgresql(df)
--- src/yfmanager.py
+++ src/yfmanager.py
@@ -4,14 +4,14 @@ import yfinance as yf
 
 class Yfmanager:
     
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.config = config
     
-    def download_companies_yf(self, companies: list, dt_days: int = 1, verbose: int= 1) -> pd.DataFrame():
+    def download_companies_yf(self, companies: list, verbose: int= 1) -> pd.DataFrame():
         df = pd.DataFrame()
 
         for company in companies:
-            data = yf.download(company, start=date.today() - timedelta(days=dt_days), end=date.today())
+            data = yf.download(company, start=date.today() - timedelta(days=self.config.days_to_fetch), end=date.today())
             data = data.reset_index()
             data["company_code"] = company
             df = pd.concat([df, data])
