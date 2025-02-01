f0ad9894af5f1952e022a53885ec76ab70bec72b
AlexanderLB98 <AlexanderBool98@gmail.com>
2025-02-01 13:18:28 +0100

    NEW: New implementation to get dataset without saving to postgre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from src.pipeline import pipeline
 
-
 if __name__ == "__main__":
-    print("Yepaaaa")
     pipeline()
+
--- src/yfmanager.py
+++ src/yfmanager.py
@@ -7,15 +7,25 @@ class Yfmanager:
     def __init__(self, config):
         self.config = config
     
-    def download_companies_yf(self, companies: list, verbose: int= 1) -> pd.DataFrame():
-        df = pd.DataFrame()
 
+    def download_companies_yf(self, companies: list, verbose: int = 1) -> pd.DataFrame():
+        # Download 2 days of data for each ticker, grouping by 'Ticker' to structure the DataFrame with multi-level columns
+        df = yf.download(companies, group_by='Ticker', start=date.today() - timedelta(days=self.config.days_to_fetch), end=date.today())
+
+        # Transform the DataFrame: stack the ticker symbols to create a multi-index (Date, Ticker), then reset the 'Ticker' level to turn it into a column
+        df = df.stack(level=0, future_stack=True).rename_axis(['Date', 'Ticker']).reset_index(level=1).reset_index()
+        return df
+
+
+    def download_companies_yf_old(self, companies: list, verbose: int= 1) -> pd.DataFrame():
+        df = pd.DataFrame()
+        
         for company in companies:
             try:
                 data = yf.download(company, start=date.today() - timedelta(days=self.config.days_to_fetch), end=date.today())
                 data = data.reset_index()
                 data["company_code"] = company
-                df = pd.concat([df, data])
+                df = pd.concat([df, data], axis=0)
             except Exception as e:
                 print(e)
             
